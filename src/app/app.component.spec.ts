96554817e251cc7af923364945c5a74ea223bdf8
diego.eslava <d.eslava@uniandes.edu.co>
2022-04-29 15:44:43 -0500

    Se corrige error en un test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import {PlantaListComponent} from "./planta/planta-list/planta-list.component";
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      imports: [RouterTestingModule, HttpClientModule],
+      declarations: [AppComponent, PlantaListComponent],
     }).compileComponents();
   });
 
